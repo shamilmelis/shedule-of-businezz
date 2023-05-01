fc1875a5832759df5dcffcf503f62e98faa2a3ea
shamilmelis <116951595+shamilmelis@users.noreply.github.com>
2023-05-01 23:43:06 +0600

    main.js

--- main.js
+++ main.js
@@ -49,4 +49,5 @@ showButton.addEventListener('click', function() {
     mainContainer.style.display = "flex";
     let infoContainer = document.getElementById('container-2__blocks__appendChild');
     infoContainer.style.display = "flex";
+    addButton.disabled = true;
 })
